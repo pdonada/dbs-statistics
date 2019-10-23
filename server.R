12e395e398afd77f3280643c1a5419656db5502f
Peter <pdonada@gmail.com>
2019-10-23 20:25:28 +0100

    Update server.R

--- server.R
+++ server.R
@@ -7,5 +7,16 @@ function(input, output) {
   output$table <- renderTable({
     head(cars, 4)
   })
+   
+  #render table PETERSON navtabb
+  output$contents <- renderDataTable({
+    inFile <- input$file1
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    read.csv(inFile$datapath, header = input$header,
+             sep = input$sep, quote = input$quote)
+  })  
   
 }
