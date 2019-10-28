87cf8e7e0b2943ef5a11969e649a98fd57415c89
Peter <pdonada@gmail.com>
2019-10-28 20:07:56 +0000

    Upload structure
    Included a combined upload structure to attend table and maps.

--- ui.R
+++ ui.R
@@ -13,72 +13,87 @@ library(sf)
 library(spData)
 library(tidyverse)
 library(tmap) 
+library(DT)
 
-
+########################################
+######Template
+########################################
 
 fluidPage(theme = shinytheme("lumen"), 
-navbarPage(    
-  "MODELS:",
-  tabPanel("Template",             
-           sidebarPanel(
-             fileInput("file", "File input:"),
-             textInput("txt", "Text input:", "general"),
-             sliderInput("slider", "Slider input:", 1, 100, 30),
-             tags$h5("Deafult actionButton:"),
-             actionButton("action", "Search"),
-           ),
-           mainPanel(
-             tabsetPanel(
-               tabPanel("Tab 1",
-                        h4("Table"),
-                        tableOutput("table"),
-                        h4("Verbatim text output"),
-                        verbatimTextOutput("txtout"),
-                        h1("Header 1"),
-                        h2("Header 2"),
-                        h3("Header 3"),
-                        h4("Header 4"),
-                        h5("Header 5")
-               ),
-               tabPanel("Tab 2", "This panel is intentionally left blank"),
-               tabPanel("Tab 3", "This panel is intentionally left blank")
-             )
-           )
-  ), ###End "tabPanel=Template"
-  
-  tabPanel("Probability", "This panel is intentionally left blank"),
-  tabPanel("Something", "This panel is intentionally left blank"),
-  tabPanel("Simulated", "This panel is intentionally left blank"),
-  
-  tabPanel("PETERSON",             
-           sidebarPanel(
-             
-             fileInput('file1', 
-                       'Choose file to upload (csv)',
-                       accept = ".csv"),
-             checkboxInput('header', 'Header', TRUE),
-             radioButtons("sep", "Separator",
-                          choices = c(Comma = ",",
-                                      Semicolon = ";"),
-                          selected = ","),
-             tags$hr(),
-             
-             sliderInput("slider", "Slider input:", 1, 100, 30),
-             tags$h5("Deafult actionButton:"),
-             actionButton("action", "Search"),
-           ),
-           
-           mainPanel(
-             tabsetPanel(
-               tabPanel("Data Sample",
-                        h5("Generate an Extructured Table from your file"),
-                        dataTableOutput("contents")
-                ),
-               tabPanel("Interactive Map",
-                      "This panel is intentionally left blank"
-                )
-              )
-           )     
-  )  ###End "tabPanel=PETERSON"
-)   ###End "navbarPage"
+          navbarPage(    
+            "MODELS:",
+            tabPanel("Template",             
+                     sidebarPanel(
+                       fileInput("file", "File input:"),
+                       textInput("txt", "Text input:", "general"),
+                       sliderInput("slider", "Slider input:", 1, 100, 30),
+                       tags$h5("Deafult actionButton:"),
+                       actionButton("action", "Search"),
+                     ),
+                     mainPanel(
+                       tabsetPanel(
+                         tabPanel("Tab 1",
+                                  h4("Table"),
+                                  tableOutput("table"),
+                                  h4("Verbatim text output"),
+                                  verbatimTextOutput("txtout"),
+                                  h1("Header 1"),
+                                  h2("Header 2"),
+                                  h3("Header 3"),
+                                  h4("Header 4"),
+                                  h5("Header 5")
+                         ),
+                         tabPanel("Tab 2", "This panel is intentionally left blank"),
+                         tabPanel("Tab 3", "This panel is intentionally left blank")
+                       )
+                     )
+            ), ###End "tabPanel=Template"
+            
+            tabPanel("Probability", "This panel is intentionally left blank"),
+            tabPanel("Something", "This panel is intentionally left blank"),
+            tabPanel("Simulated", "This panel is intentionally left blank"),
+            
+########################################
+######PETERSON
+########################################
+            
+            tabPanel("PETERSON",             
+                     sidebarPanel(
+                       
+                       fileInput('file1', 
+                                 'Choose file to upload (csv)',
+                                 accept = ".csv"),
+                       checkboxInput('header', 'Header', TRUE),
+                       radioButtons("sep", "Separator",
+                                    choices = c(Comma = ",",
+                                                Semicolon = ";"),
+                                    selected = ","),
+                       radioButtons('quote', 'Quote',
+                                   c(None='',
+                                     'Double Quote'='"',
+                                     'Single Quote'="'"),
+                                   '"'),
+                       tags$hr(),
+                       
+                       sliderInput("slider", "Slider input:", 1, 100, 30),
+                       tags$h5("Deafult actionButton:"),
+                       actionButton("action", "Search"),
+                     ),
+                     
+                     mainPanel(
+                       tabsetPanel(
+                         tabPanel("Data Sample",
+                                  h5("Generate an Extructured Table from your file"),
+                                  DTOutput("contents")
+                         ),
+                         
+                         tabPanel("Interactive Map",
+                                  h5("Generate Maps"),
+                                  leafletOutput(outputId = "mymap")
+                                    )
+                         )
+                       )
+                          
+            )  ###End "tabPanel=PETERSON"
+          )   ###End "navbarPage"
 )   ###End "fluidPage"
